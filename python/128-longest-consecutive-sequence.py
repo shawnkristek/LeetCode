24898293a308215c72001bd957aaf63d3f20ec5b
Shawn Kristek <shawnkristek@gmail.com>
2022-07-03 13:01:13 -0600

    solution for 128, improved testing method

---
+++ python/128-longest-consecutive-sequence.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestConsecutive(nums: list[int]) -> int:
+        # create set from nums
+        numSet = set(nums)
+        longest = 0
+        
+        # loop thru nums checking and counting consecutive elements
+        for n in nums:
+            if (n-1) not in numSet:
+                length = 1
+                while n + length in numSet:
+                    length += 1
+                longest = max(longest, length)
+                
+        return longest
+
+# test
+cases = [
+  ([100,4,200,1,3,2], 4),
+  ([0,3,7,2,5,8,4,6,0,1], 9)
+]
+
+for case, solution in cases:
+  print(Solution.longestConsecutive(case) == solution)
