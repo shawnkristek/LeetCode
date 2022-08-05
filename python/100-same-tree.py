8673c03389d431f6dd5c65286b9b7d77f9777ed0
BSwole <shawnkristek@gmail.com>
2022-08-05 10:27:52 -0600

    solution for 100

---
+++ python/100-same-tree.py
@@ -0,0 +1,28 @@
+from reuse import TreeNode
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        
+        def dfs(p: TreeNode, q: TreeNode) -> bool:
+            # both null
+            if not p and not q:
+                return True
+
+            if p and q and p.val == q.val:
+                return dfs(p.left, q.left) and dfs(p.right, q.right)
+
+            return False
+
+        return dfs(p, q)
+
+# test
+tests = [
+    (TreeNode().build([1,2,3]), TreeNode().build([1,2,3]),    True),
+    (TreeNode().build([1,2]),   TreeNode().build([1,None,2]), False),
+    (TreeNode().build([1,2,1]), TreeNode().build([1,1,2]),    False),
+]
+
+for p,q,solution in tests:
+    sol = Solution()
+    sol = sol.isSameTree(p,q)
+    print( sol == solution )
