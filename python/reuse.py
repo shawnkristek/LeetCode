8b0a06c294cb112c6af001a6b7531ae720020b9e
BSwole <shawnkristek@gmail.com>
2022-08-11 08:53:54 -0600

    TrieNode added to reuse module

--- python/reuse.py
+++ python/reuse.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -95,3 +95,46 @@ class TreeNode:
                 q.append(curr.right)
 
         return head
+
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict()
+        self.eow = False
+
+    def __traverse(self, s: str) : # -> tuple(bool, 'Trie')
+        letters = deque(s)
+        node = self
+
+        while letters:
+            c = letters.popleft()
+            if c in node.children:
+                node = node.children[c]
+            else:
+                return (False, node)
+
+        return (True, node)
+
+    def insert(self, word: str) -> None:
+        letters = deque(word)
+        node = self
+
+        while letters:
+            c = letters.popleft()
+            if c in node.children:
+                node = node.children[c]
+            else:
+                node.children[c] = TrieNode()
+                node = node.children[c]
+
+        # last node is end-of-word
+        node.eow = True
+
+    def search(self, word: str) -> bool:
+        found, node = self.__traverse(word)
+
+        return node.eow and found 
+
+    def startsWith(self, prefix: str) -> bool:
+        found, node = self.__traverse(prefix)
+
+        return found
