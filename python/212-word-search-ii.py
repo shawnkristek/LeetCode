028d7fe83e9dc5477d98784647a32d19371a5ffb
BSwole <shawnkristek@gmail.com>
2022-08-12 07:37:45 -0600

    init 212 solution

--- python/212-word-search-ii.py
+++ python/212-word-search-ii.py
@@ -1,8 +1,8 @@
-from reuse import Trie
+from reuse import TrieNode
 
 class Solution:
     def __init__(self):
-        self.root = Trie()
+        self.root = TrieNode()
         self.board = []
 
     def __convertBoard(self):
@@ -13,10 +13,74 @@ class Solution:
         self.__convertBoard()
 
 
-        pass
 
 
+class NeetNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+        self.refs = 0
 
+    def addWord(self, word):
+        cur = self
+        cur.refs += 1
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = NeetNode()
+            cur = cur.children[c]
+            cur.refs += 1
+        cur.isWord = True
+
+    def removeWord(self, word):
+        cur = self
+        cur.refs -= 1
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+                cur.refs -= 1
+
+
+class NeetSolution:
+    def findWords(self, board: list[list[str]], words: list[str]) -> list[str]:
+        root = NeetNode()
+        for w in words:
+            root.addWord(w)
+
+        ROWS, COLS = len(board), len(board[0])
+        res, visit = set(), set()
+
+        def dfs(self, r, c, node, word):
+            if (
+                r < 0
+                or c < 0
+                or r == ROWS
+                or c == COLS
+                or board[r][c] not in node.children
+                or node.children[board[r][c]].refs < 1
+                or (r, c) in visit
+            ):
+                return
+
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                node.isWord = False
+                res.add(word)
+                root.removeWord(word)
+
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            visit.remove((r, c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+
+        print(res)
+        return list(res)
 # test
 tests = [ 
     (
