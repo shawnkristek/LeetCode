4e2b49347f107087112d4a5ecab3eab575cc9ea3
Shawn Kristek <shawnkristek@gmail.com>
2022-09-30 23:40:32 -0600

    rename files to match main

--- python/reuse.py
+++ python/reuse.py
@@ -1,4 +1,4 @@
-from collections import deque
+import os
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -29,69 +29,9 @@ class ListNode:
 
         return next
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-    def values(self):
-        values = []
-        q = deque([self])
-        
-        while q:
-            # pop next node for processing
-            curr = q.popleft()
-
-            if curr:
-                # append node value
-                values.append(curr.val)
-    
-                # add children to stack for processing
-                if curr.left:
-                    q.append(curr.left)
-                if curr.right:
-                    # if right child only
-                    if not curr.left:
-                        q.append(None)
-                    q.append(curr.right)
-            else:
-                values.append(None)
-
-
-        return values
-
-    def __repr__(self):
-        return str(self.values())
-
-
-    def build(self, values: list[int]) -> 'TreeNode':
-        if not values:
-            return None
-
-        head = TreeNode(values.pop(0), None, None)
-        q = deque([head])
-
-        while q:
-            # pop next node for updates
-            curr = q.popleft()
-
-            # update left,right pointers of current node
-            if values:
-                if values[0] is None:
-                    values.pop(0)
-                else:
-                    curr.left = TreeNode(values.pop(0), None, None)
-            if values:
-                if values[0] is None:
-                    values.pop(0)
-                else:
-                    curr.right = TreeNode(values.pop(0), None, None)
-
-            # append new nodes to q
-            if curr.left:
-                q.append(curr.left)
-            if curr.right:
-                q.append(curr.right)
-
-        return head
+def rename():
+    for file in os.listdir('.'):
+        if file.endswith('.py'):
+            if file.find('-') > -1:
+                new = file.replace('-','_')
+                os.rename(file, new)
