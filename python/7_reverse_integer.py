e680f5f394f079bbbc0fbda713bf01d60c9d3d89
BSwole <shawnkristek@gmail.com>
2022-10-02 14:05:56 -0600

    solution for 7

--- python/7_reverse_integer.py
+++ python/7_reverse_integer.py
@@ -1,14 +1,35 @@
-from math import fmod
+import math
 
-class Solution:
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         res = abs(x)
+
+
+#         if res > pow(2,31) - 1 or res < pow(-2,31):
+#             return 0
+
+#         return res if x >= 0 else -res
+
+class NeetSolution:
     def reverse(self, x: int) -> int:
-        res = abs(x)
+        MAX_INT =  2147483647
+        MIN_INT = -2147483648
 
+        result =  0
 
-        if res > pow(2,31) - 1 or res < pow(-2,31):
-            return 0
+        while x:
+            digit = int(math.fmod(x, 10))
+            x = int(x/10)
 
-        return res if x >= 0 else -res
+            if result > MAX_INT // 10 or (result == MAX_INT // 10 and digit >= MAX_INT % 10):
+                return 0
+            if result < MIN_INT // 10 or (result == MIN_INT // 10 and digit <= MIN_INT % 10):
+                return 0
+
+            result = (result * 10) + digit
+
+        return result
+
 
 # test
 
@@ -28,6 +49,6 @@ tests = [
 ]
 
 for x,solution in tests:
-    sol = Solution()
+    sol = NeetSolution()
     sol = sol.reverse(x)
     print( sol == solution )
